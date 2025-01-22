205bfa7732b9c4881e1683a172c01cfe35014910
CHEN WEI-CHENG <3331363@gmail.com>
2025-01-22 22:41:42 +0800

    2025.01.22

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # 互動網頁設計
 
-
-
-
+-   PPT 裡面得程式碼：https://github.com/kcwc1029/blog-web
---
+++ js&tf/Viewer4PHP/Ch13/getDateTime.php
@@ -0,0 +1,18 @@
+﻿<?php
+// 關閉錯誤訊息
+error_reporting(0);
+// 取得欄位值
+$name = (isset($_POST["name"]) ) ? $_POST["name"] : $_GET["name"];
+$type = (isset($_POST["type"]) ) ? $_POST["type"] : $_GET["type"];
+if ($type == "date")
+   $dt = date("m/j/Y");
+else
+   $dt = date("h:i:s A");
+header('Content-type: application/json');
+$response = array();
+$response[0] = array(
+     'date' => $dt,
+     'name'=> $name
+);
+echo json_encode($response);
+?>  
